ff68d6c1d41f8013afe35cfa26cd32e27a8f8c4a
Mark Kennard <komcomputers@gmail.com>
2024-01-13 20:33:52 +1300

    s5p6442: Added base 256MB RAM for Apollo board

--- arch/arm/mach-s5p6442/s5p6442.c
+++ arch/arm/mach-s5p6442/s5p6442.c
@@ -1,19 +1,24 @@
 // SPDX-License-Identifier: GPL-2.0
 //
-// Samsung's S5PC110/S5PV210 flattened device tree enabled machine.
+// Samsung S5P6442 based machine.
+//
+// Based on S5PV210 device tree
 //
 // Copyright (c) 2013-2014 Samsung Electronics Co., Ltd.
 // Mateusz Krawczuk <m.krawczuk@partner.samsung.com>
 // Tomasz Figa <t.figa@samsung.com>
+// Mark Kennard <markkennard4@gmail.com>
 
 #include <linux/of_fdt.h>
 #include <linux/platform_device.h>
+#include <linux/memblock.h>
 
 #include <asm/mach/arch.h>
 #include <asm/mach/map.h>
 #include <asm/system_misc.h>
 
 #include "common.h"
+#include "s5p6442.h"
 #include "regs-clock.h"
 
 static int __init s5pv210_fdt_map_sys(unsigned long node, const char *uname,
@@ -39,33 +44,39 @@ static int __init s5pv210_fdt_map_sys(unsigned long node, const char *uname,
 	return 1;
 }
 
-static void __init s5pv210_dt_map_io(void)
+static void __init s5p6442_dt_map_io(void)
 {
 	debug_ll_io_init();
 
 	of_scan_flat_dt(s5pv210_fdt_map_sys, NULL);
 }
 
-static void s5pv210_dt_restart(enum reboot_mode mode, const char *cmd)
+static void s5p6442_dt_restart(enum reboot_mode mode, const char *cmd)
 {
 	__raw_writel(0x1, S5P_SWRESET);
 }
 
-static void __init s5pv210_dt_init_late(void)
+static void __init s5p6442_dt_init_late(void)
 {
 	platform_device_register_simple("s5pv210-cpufreq", -1, NULL, 0);
 	s5pv210_pm_init();
 }
 
-static char const *const s5pv210_dt_compat[] __initconst = {
-	"samsung,s5pc110",
-	"samsung,s5pv210",
+static char const *const s5p6442_dt_compat[] __initconst = {
+	"samsung,s5p6442",
 	NULL
 };
 
-DT_MACHINE_START(S5PV210_DT, "Samsung S5PC110/S5PV210-based board")
-	.dt_compat = s5pv210_dt_compat,
-	.map_io = s5pv210_dt_map_io,
-	.restart = s5pv210_dt_restart,
-	.init_late = s5pv210_dt_init_late,
+void __init s5p6442_fixup(struct tag *t, char **from)
+{
+	memblock_add(PHYS_OFFSET, APOLLO_PHYS_SIZE_DDR);
+}
+
+DT_MACHINE_START(S5P6442_DT, "Samsung S5P6442-based board")
+	.fixup = s5p6442_fixup,
+	.dt_compat = s5p6442_dt_compat,
+	.map_io = s5p6442_dt_map_io,
+	.restart = s5p6442_dt_restart,
+	.init_late = s5p6442_dt_init_late,
+	.nr = APOLLO_MACHID,
 MACHINE_END
---
+++ arch/arm/mach-s5p6442/s5p6442.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2024 Mark Kennard
+ *		markkennard4@gmail.com
+ *
+ * Header for S5P6442 machines
+ */
+
+#include <linux/sizes.h>
+
+#ifndef __ARCH_ARM_MACH_S5P6442_S5P6442_H
+#define __ARCH_ARM_MACH_S5P6442_S5P6442_H
+
+#define APOLLO_MACHID   0x00000914     // DTB ID for Galaxy Apollo i5800
+#define APOLLO_PHYS_SIZE_DDR    SZ_256M
+
+#endif /* __ARCH_ARM_MACH_S5P6442_COMMON_H */
